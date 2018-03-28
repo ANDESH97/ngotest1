e88a3daf8b6d43a36ae2377d309de173523f91bd
Ankit Deshmukh <iankitdeshmukh2@gmail.com>
2018-03-29 02:04:52 +0530

    Table UI Done

---
+++ ngotest1/Controllers/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  ngotest1
+//
+//  Created by Ankit Deshmukh on 29/03/18.
+//  Copyright © 2018 Wayne Corporations Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var ngolabel: UILabel!
+    @IBOutlet weak var caption: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+    
+}
